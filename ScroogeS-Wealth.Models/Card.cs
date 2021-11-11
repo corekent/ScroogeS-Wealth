2b3fb88a7322ff74b8fa7bfeca45f6723b393ebd
Алексей Коровин <bennykorovin@gmail.com>
2021-11-11 22:20:13 +0300

    Card User Workspace classes and their properties were created

---
+++ ScroogeS-Wealth.Models/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScroogeS_Wealth.Models
+{
+    public class Card
+    {
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        
+    }
+}
---
+++ ScroogeS-Wealth.Models/WorkSpace.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScroogeS_Wealth.Models
+{
+    public class WorkSpace
+    {
+        public User GeneralUser { get; set; }
+        public List<Card> Cards { get; set; }
+    }
+}
