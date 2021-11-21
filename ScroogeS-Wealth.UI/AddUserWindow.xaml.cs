c2f3dd4107a23d35ecb01c8d2e75299076230f13
Алексей Коровин <bennykorovin@gmail.com>
2021-11-21 19:49:38 +0300

    Bad combobox was added

--- ScroogeS-Wealth.UI/AddUserWindow.xaml.cs
+++ ScroogeS-Wealth.UI/AddUserWindow.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using ScroogeS_Wealth.Business;
 using ScroogeS_Wealth.Models;
+using ScroogeS_Wealth.Storage;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ScroogeS_Wealth.UI
 {
@@ -37,6 +29,11 @@ namespace ScroogeS_Wealth.UI
                 user.CreateUser(userName);
                 MessageBox.Show("Пользователь успешно добавлен! =)");
             }
+        }
+
+        private void AddUserToMainWindow()
+        {
+            
         }
 
         private void Button_Close_Click(object sender, RoutedEventArgs e)
--- ScroogeS-Wealth.UI/MainWindow.xaml.cs
+++ ScroogeS-Wealth.UI/MainWindow.xaml.cs
@@ -2,19 +2,9 @@
 using ScroogeS_Wealth.Models;
 using ScroogeS_Wealth.Storage;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace ScroogeS_Wealth.UI
 {
@@ -23,9 +13,14 @@ namespace ScroogeS_Wealth.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<User> Names { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            GenericStorage<User> userStorage = new GenericStorage<User>();
+            var names = userStorage.Get();
+            Names = new ObservableCollection<User>(names);
+            DataContext = Names;
         }
 
         private void Button_TransitionToCreateUserWindow_Click(object sender, RoutedEventArgs e)
